49cecc8f688647771cf88edafe662d3339e1a876
Asha <asha@youthradio.org>
2013-03-08 19:17:25 -0800

    Daily List Now Emails
    This change:
    Allows styled information to send in an email through gmail.
    Removed the attachment from the email template.
    Added a success screen to review information.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,32 @@
 from flask import Flask, render_template
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email import Encoders
+import os
+
+gmail_user = "scripts@youthradio.org"
+gmail_pwd = "TrBDXGZ9"
+
+def mail(to, subject, text):
+    msg = MIMEMultipart()
+
+    msg['From'] = gmail_user
+    msg['To'] = to
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(text, 'html'))
+
+    mailServer = smtplib.SMTP("smtp.gmail.com", 587)
+    mailServer.ehlo()
+    mailServer.starttls()
+    mailServer.ehlo()
+    mailServer.login(gmail_user, gmail_pwd)
+    mailServer.sendmail(gmail_user, to, msg.as_string())
+    # Should be mailServer.quit(), but that crashes...
+    mailServer.close()
+
 
 app = Flask(__name__)
 
@@ -9,7 +37,9 @@ def form():
 
 @app.route("/submit", methods=['POST'])
 def submission():
+	mail("applab@youthradio.org", "Daily List Test", render_template('response.html'))
 	return render_template('response.html')
+
 
 
 if __name__ == "__main__":
